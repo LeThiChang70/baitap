7929654ad06a0074ad6ad77477c4c45525bedd55
2 Chang Lê Thị <1821051070@student.humg.edu.vn>
2021-10-16 19:59:16 +0700

    First_commit_LTC070

---
+++ LeThiChang070/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeThiChang070.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

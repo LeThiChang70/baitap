887543e86caccf8fdc63251888fc4674c03a37f3
2 Chang Lê Thị <1821051070@student.humg.edu.vn>
2021-10-17 22:11:53 +0700

    Final_Commit_LTC070

---
+++ LeThiChang070/Models/StringProcessLTC070.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeThiChang070.Models
+{
+    public class StringProcessLTC070
+    {
+        public void ToUpper (String StrInput)
+        {
+            string HoTen = string.Empty;
+            HoTen = Console.ReadLine();
+            HoTen = HoTen.ToUpper();
+          
+        }
+    }
+}
